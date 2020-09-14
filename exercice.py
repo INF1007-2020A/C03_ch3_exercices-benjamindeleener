46a9a22abc831b2bbd9aed68520c7561302b1434
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2020-09-14 15:27:20 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -10,21 +10,21 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    a = 6
-    
-    return 0.0
+    return (angle_degs + (angle_mins + angle_secs/60)/60) * math.pi/180
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    angle = (angle_rads * 360) / (2 * math.pi)
+    degrees, minutes, seconds = math.floor(angle), angle // 60, math.floor(angle/3600)
+    return degrees, minutes, seconds
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) / 1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature * 1.8 + 32
 
 
 def main() -> None:
@@ -36,7 +36,7 @@ def main() -> None:
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
 
     print(f"Conversion de 100 Celsius en Farenheit: {to_farenheit(100.0)}")
-    print(f"Conversion de 451 Farenheit en Celsius: {to_celsius(451.0)}")
+    print(f"Conversion de 212 Farenheit en Celsius: {to_celsius(212.0)}")
 
 
 if __name__ == '__main__':
